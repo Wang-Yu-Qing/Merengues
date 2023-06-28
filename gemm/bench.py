d5ff599260e851c11ca72d6ee8db49ef91c99781
413022472@qq.com <413022472@qq.com>
2023-06-28 23:06:35 +0800

    trans and block

--- gemm/bench.py
+++ gemm/bench.py
@@ -1,26 +1,14 @@
 import numpy as np
 import time
 
-m = 2048
-n = 2048
-k = 4056
-flop = 2 * m * k * n # 2 is for multi and sum
-a = np.random.random((m, n))
-b = np.random.random((n, k))
+# 125.0000 GFLOPS for numpy
+n = 1000
+flop = 2 * n * n * n # 2 is for multi and sum
+a = np.random.random((n, n))
+b = np.random.random((n, n))
 
 tic = time.monotonic()
 a @ b
 toc = time.monotonic()
 
-print("{:.4f} TFLOPS".format(flop / (toc - tic) * 1e-12))
-
-"""
-(n, n)
-origin: n * n * n * 2 = 2 * n^3
-
-b = 4
-
-4 * (n / 4, n / 4)
-now: (n / 4) * (n / 4) * (n / 4) * 2 * 4 = (2 / 4^2) * n^3
-
-"""
+print("{:.4f} GFLOPS".format(flop / (toc - tic) * 1e-9))
--- gemm/gemm.cpp
+++ gemm/gemm.cpp
@@ -3,9 +3,10 @@
 #include <time.h>
 #include <random>
 
-#define N 10
+// N should be big enough (ensure all matrix cannot fit into cache)
+#define N 1000
+#define BLOCK_SIZE 100
 
-const int K = 10;
 float A[N][N], B[N][N], C[N][N];
 
 void initMat() {
@@ -23,17 +24,18 @@ uint64_t nanos() {
   return (uint64_t)start.tv_sec*1000000000 + (uint64_t)start.tv_nsec;
 }
 
-void printMat() {
+void printMat(float (&M)[N][N]) {
   printf("[\n");
   for (int i = 0; i < N; i++) {
     printf(" [");
     for (int j = 0; j < N; j++) {
-      j == N - 1 ? printf("%f],\n", C[i][j]) : printf("%f, ", C[i][j]);
+      j == N - 1 ? printf("%f],\n", M[i][j]) : printf("%f, ", M[i][j]);
     }
   }
   printf("]\n");
 }
 
+// 0.545400 GFLOPS
 void naive() {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
@@ -44,18 +46,49 @@ void naive() {
   }
 }
 
+// 0.926365 GFLOPS, 在c++中，array是按行存储的，因此按行方向遍历将充分利用缓存, https://www.bilibili.com/video/BV1cU4y1C7xY/?spm_id_from=333.999.0.0&vd_source=1edd1b65ec7bcde5814606bd07bca876
+void trans() {
+  // TODO: real transpose
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < N; k++) {
+        C[i][j] += A[i][k] * B[j][k];
+      }
+    }
+  }
+}
+
+/* 
+  0.956145 GFLOPS
+  如果一行大到缓存吃不下，那么block不会比trans更好，否则block更好
+  因为只有在能吃下block的情况下，缓存的数据才多于trans
+*/
+void blockTrans() {
+  for (int bi = 0; bi < N; bi += BLOCK_SIZE) {
+    for (int bj = 0; bj < N; bj += BLOCK_SIZE) {
+      for (int i = bi; i < bi + BLOCK_SIZE; i++) {
+        for (int j = bj; j < bj + BLOCK_SIZE; j++) {
+          for (int k = 0; k < N; k++) {
+            C[i][j] += A[i][k] * B[j][k];
+          }
+        }
+      }
+    }
+  }
+}
+
 int main() {
   initMat();
 
   uint64_t tic = nanos();
-  for (int i = 0; i < K; i++) {
-    naive();
-  }
+  //naive();
+  //trans();
+  blockTrans();
   uint64_t toc = nanos();
 
-  printf("%f GFLOPS\n", (K * N * N * N * 2 * 1e-9) / ((toc - tic) * 1e-9));
+  printf("%f GFLOPS\n", (N * N * N * 2 * 1e-9) / ((toc - tic) * 1e-9));
 
-  printMat();
+  //printMat(C);
 
   return 0;
 }
